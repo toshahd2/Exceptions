4063c6f182a68aa86d1bdd3e75e285f77c05fb94
Anton <tosha.hd2@gmail.com>
2023-04-28 15:54:52 +0300

    task2 update

--- src/hw1/task2.java
+++ src/hw1/task2.java
@@ -3,15 +3,25 @@ package hw1;
 import java.util.Arrays;
 
 public class task2 {
+    /**
+     Реализуйте метод, принимающий в качестве аргументов два целочисленных массива,
+     и возвращающий новый массив, каждый элемент которого равен разности элементов
+     двух входящих массивов в той же ячейке.
+     Если длины массивов не равны, необходимо как-то оповестить пользователя.
+    */
+
     public static void main(String[] args) {
         int[] ar1 = {10,9,8,7,6,5,4,3,2,1,0};
         int[] ar2 = {0,1,2,3,4,5,6,7,8,9};
+        // int[] ar2 = null;
         int[] arSub = SubArrayElements(ar1,ar2);
         System.out.println(Arrays.toString(arSub));
     }
 
     private static int[] SubArrayElements (int[] array1, int[] array2){
-        if (array1.length != array2.length) {
+        if (array1 == null || array2 == null) {
+            throw new RuntimeException("Одна из переменных равна  null");
+        } else if (array1.length != array2.length){
             throw new RuntimeException("Разная длина массивов");
         }
 
